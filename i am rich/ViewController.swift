77280481eb5a3c820d8c46d37a4148c83ee7ae82
Guilherme Ytalo <gydaytalo@gmail.com>
2022-09-11 23:17:20 -0300

    Initial Commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Guilherme Ytalo on 11/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
